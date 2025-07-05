9e8d49a1246f5111fafa1c411a513cc499a327a5
vaishtables <vaishnavy.bp@gmail.com>
2025-07-05 14:20:42 -0400

    Update configuration

--- pelicanconf.py
+++ pelicanconf.py
@@ -19,6 +19,7 @@ MARKDOWN = {
         "plugins.citekeyx",
         "markdown.extensions.def_list",
         "markdown.extensions.tables",
+        "markdown.extensions.codehilite",
         "pymdownx.tasklist",
     ],
     "extension_configs": {
